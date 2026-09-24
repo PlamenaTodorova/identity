4c4c0ed52e447c911f24e1f177feb1cbd1c43316
PlamenaTodorova <plamena.i.todorova@gmail.com>
2026-09-24 17:15:18 +0300

    fix boot issue

--- api/src/main/java/com/identity/api/IdentityApplication.java
+++ api/src/main/java/com/identity/api/IdentityApplication.java
@@ -1,11 +1,17 @@
 package com.identity.api;
 
 import com.identity.core.config.CoreConfig;
+import com.identity.core.domain.User;
+import com.identity.core.repository.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.context.annotation.Import;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.identity")
+@EntityScan(basePackageClasses = User.class)
+@EnableJpaRepositories(basePackageClasses = UserRepository.class)
 @Import(CoreConfig.class)
 public class IdentityApplication {
 
